dd4c28bcf3595a144408a5d25d586b3375233b59
Daeki <kimdaeki@gmail.com>
2024-01-18 12:44:47 +0900

    mem1

---
+++ src/main/java/com/winter/app/member/MemberDAO.java
@@ -0,0 +1,25 @@
+package com.winter.app.member;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private final String NAMESPACE="com.winter.app.member.MemberDAO.";
+	
+	
+	public int setJoin(MemberDTO memberDTO) throws Exception {
+		return sqlSession.insert(NAMESPACE+"setJoin", memberDTO);
+	}
+	
+	public int setAvatarAdd(AvatarDTO avatarDTO) throws Exception {
+		return sqlSession.insert(NAMESPACE+"setAvatarAdd", avatarDTO);
+	}
+	
+
+}
